ee3692a2b6ef2942abb407673cee76579d50a3c7
Henry <hvelas93@gmail.com>
2021-04-23 13:33:45 -0400

    finished

--- src/profile.js
+++ src/profile.js
@@ -22,7 +22,32 @@ function addEditButton() {
     editProfileButtonElement.dataset.type = "button"
     editProfileButtonElement.innerText = "Edit Your Profile"
     editProfileButtonElement.addEventListener('click', event => {
-        
+        renderEditProfileForm()
     })
     dynamicContentBody.append(editProfileButtonElement)
+}
+
+function renderEditProfileForm() {
+    dynamicContentBody.innerHTML= ""
+    let formElement = document.createElement('form')
+    formElement.innerHTML =`
+    <h4>Enter desired changes below</h4>
+    <input type="text" name="name" placeholder="Enter Name" value="" />
+    <input type="text" name="email" placeholder="Enter Email" value="" />
+    <input type="text" name="password" placeholder="New Password" value="" />
+    <input type="integer" name="age" placeholder="Age" value="" />
+    <input type="integer" name="height_in_inches" placeholder="Height (inches)" value="" />
+    <input type="integer" name="weight_in_lbs" placeholder="Weight (lbs)" value="" />
+    <input type="submit" value="Submit Changes" />
+    `
+  formElement.addEventListener('submit', event =>{
+        event.preventDefault()
+        updateProfile(event)
+    })
+    dynamicContentBody.append(formElement)
+
+}
+
+function updateProfile(event) {
+    console.log(event.target.name.value)
 }
